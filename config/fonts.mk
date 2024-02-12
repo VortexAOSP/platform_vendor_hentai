d903571e2642c51410e73d73a6be18b2eda0687a
oberdfr <alebennella@gmail.com>
2024-02-12 22:19:36 +0100

    vendor: Add font configuration for VortexThemePicker

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,26 +1,6 @@
-#
-# Copyright (C) 2021 Raphielscape LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
-
-PRODUCT_PACKAGES += \
-    HarmonyOS-Sans-Italic.ttf \
-    HarmonyOS-Sans.ttf
+# Fonts
+LOCAL_PATH := vendor/vortex/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
 # Register vendor fonts
 PRODUCT_COPY_FILES += vendor/vortex/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
