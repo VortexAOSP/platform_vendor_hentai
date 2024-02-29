55b94889e656f3a8d9724b22a5477884ebfa04b4
oberdfr <alebennella@gmail.com>
2024-02-29 15:32:47 +0100

    vortex: add FaceUnlock support

--- config/common.mk
+++ config/common.mk
@@ -82,6 +82,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/vortex/overlay
 
